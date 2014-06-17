cf3ac8917e5a04b7d0ca78001ce2432135735962
Meenakumarir4 <Meenakumarir4@users.noreply.github.com>
2014-06-17 11:16:05 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,12 @@
-## Put comments here that give an overall description of what your
-## functions do
+## function to create special matrix
 
-## Write a short comment describing this function
-
+## function can catch its inverse
 makeCacheMatrix <- function(x = matrix()) {
 
+mdat <- matrix(c(1,2,3, 11,12,13), nrow = 2, ncol = 3, byrow = TRUE,
+               dimnames = list(c("row1", "row2"),
+                               c("C.1", "C.2", "C.3")))
+                               mdat
 }
 
 
